8390652be4af3439c243743e78813445f87d0a4e
nicolasgenova <nicolas71ggenova@outlook.com>
2023-10-27 20:49:41 -0300

    Criação do Projeto

--- Loja/Loja.cpp
+++ Loja/Loja.cpp
@@ -1,20 +1,243 @@
-// Loja.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
-//
-
 #include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <cstring>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#pragma warning(disable : 4996)
+using namespace std;
+
+FILE* ArqProduto;
+
+typedef struct Produto {
+	char Marca[20];
+	char Descricao[30];
+	long int Codigo;
+	int Quantidade;
+	float PC, PV;
+} RegProduto;
+
+RegProduto Prod;
+
+void Menu();
+void Limpar();
+void Cadastrar();
+void Relatorio();
+void Busca();
+
+
+int main(int argc, char** argv) {
+	setlocale(LC_ALL, "Portuguese");
+	Menu();
+	return 0;
 }
 
-// Executar programa: Ctrl + F5 ou Menu Depurar > Iniciar Sem Depuração
-// Depurar programa: F5 ou menu Depurar > Iniciar Depuração
+void Cadastrar() {
+	char opcao;
+	ArqProduto = fopen("arquivo.dat", "ab");
+	if (ArqProduto == NULL) {
+		cout << "Erro na abertura do arquivo\n";
+		system("pause");
+		exit(1);
+	}
 
-// Dicas para Começar: 
-//   1. Use a janela do Gerenciador de Soluções para adicionar/gerenciar arquivos
-//   2. Use a janela do Team Explorer para conectar-se ao controle do código-fonte
-//   3. Use a janela de Saída para ver mensagens de saída do build e outras mensagens
-//   4. Use a janela Lista de Erros para exibir erros
-//   5. Ir Para o Projeto > Adicionar Novo Item para criar novos arquivos de código, ou Projeto > Adicionar Item Existente para adicionar arquivos de código existentes ao projeto
-//   6. No futuro, para abrir este projeto novamente, vá para Arquivo > Abrir > Projeto e selecione o arquivo. sln
+	do {
+		cout << "\n **********************************************************************************************************************\n";
+		cout << "\n                                         --- ADICIONAR UM NOVO PRODUTO ---";
+		cout << "\n";
+		cout << "\n **********************************************************************************************************************\n";
+		cout << "\n Código do Produto: ";
+		cin >> Prod.Codigo;
+		if (Prod.Codigo == 0) {
+			cout << " !!! Campo Código vazio. Digite novamente: ";
+			cin >> Prod.Codigo;
+		}
+		cin.ignore(80, '\n');
+		cout << " Marca: ";
+		cin.getline(Prod.Marca, sizeof(Prod.Marca));
+		if (strlen(Prod.Marca) == 0) {
+			cout << " !!! Campo Marca vazio. Digite novamente: ";
+			cin.getline(Prod.Marca, sizeof(Prod.Marca));
+		}
+		cout << " Descrição: ";
+		cin.getline(Prod.Descricao, sizeof(Prod.Descricao));
+		if (strlen(Prod.Descricao) == 0) {
+			cout << " !!! Campo Descrição vazio. Digite novamente: ";
+			cin.getline(Prod.Descricao, sizeof(Prod.Descricao));
+		}
+		cout << " Quantidade: ";
+		cin >> Prod.Quantidade;
+		if (Prod.Quantidade == 0) {
+			cout << " !!! Campo Quantidade vazio. Digite novamente: ";
+			cin >> Prod.Quantidade;
+		}
+		cout << " Preço de Custo: ";
+		cin >> Prod.PC;
+		if (Prod.PC == 0) {
+			cout << " !!! Campo Preço de Custo vazio. Digite novamente: ";
+			cin >> Prod.PC;
+		}
+		cout << " Preço de Venda: ";
+		cin >> Prod.PV;
+		if (Prod.PV == 0) {
+			cout << " !!! Campo Preço de Venda vazio. Digite novamente: ";
+			cin >> Prod.PV;
+		}
+
+		fwrite(&Prod, sizeof(Prod), 1, ArqProduto);
+		cout << "Adicionar outro produto (S/N)?";
+		cin >> opcao;
+
+	} while (toupper(opcao) == 'S');
+	fclose(ArqProduto);
+	system("cls");
+}
+
+void Relatorio() {
+	char opcao;
+	system("cls");
+
+	ArqProduto = fopen("arquivo.dat", "rb");
+	if (ArqProduto == NULL) {
+		cout << "Erro na abertura do arquivo\n";
+		system("pause");
+		exit(1);
+	}
+	cout << "\n **********************************************************************************************************************\n";
+	cout << "\n                                           --- PRODUTOS CADASTRADOS ---";
+	cout << "\n";
+	cout << "\n **********************************************************************************************************************\n";
+	cout << setw(8) << "Código";
+	cout << setw(14) << "Marca";
+	cout << setw(20) << "Descrição";
+	cout << setw(14) << "Quantidade";
+	cout << setw(16) << "Preço Custo";
+	cout << setw(16) << "Preço Venda";
+	cout << setw(20) << "Data de Cadastro";
+
+
+	while (fread(&Prod, sizeof(Prod), 1, ArqProduto) != NULL) {
+		cout << "\n";
+		cout << setw(8) << Prod.Codigo;
+		cout << setw(14) << Prod.Marca;
+		cout << setw(20) << Prod.Descricao;
+		cout << setw(14) << Prod.Quantidade;
+		cout << setw(16) << Prod.PC;
+		cout << setw(16) << Prod.PV;
+
+		time_t currentTime = time(0);
+		char* currentDate = ctime(&currentTime);
+		currentDate[strlen(currentDate) - 1] = '\0';
+
+		cout << setw(28) << currentDate;
+	}
+	fclose(ArqProduto);
+}
+
+long int BuscarProduto(int cod) {
+	char opcao;
+	system("cls");
+	ArqProduto = fopen("arquivo.dat", "rb");
+	if (ArqProduto == NULL) {
+		cout << "Erro na abertura do arquivo\n";
+		system("pause");
+		exit(1);
+	}
+	while (fread(&Prod, sizeof(Prod), 1, ArqProduto) != NULL) {
+		if (Prod.Codigo == cod) {
+			int posicao = ftell(ArqProduto) - sizeof(Prod);
+			fclose(ArqProduto);
+			return posicao;
+		}
+	}
+	fclose(ArqProduto);
+	system("cls");
+	return -1;
+}
+
+void Busca() {
+	int c;
+	long int pos;
+	cout << "\n\nQual o código que deseja encontrar: ";
+	cin >> c;
+	pos = BuscarProduto(c);
+	if (pos == -1) {
+		cout << "\nProduto não cadastrado";
+	}
+	else {
+		ArqProduto = fopen("arquivo.dat", "rb");
+		fseek(ArqProduto, pos, 0);
+		fread(&Prod, sizeof(Prod), 1, ArqProduto);
+		cout << "\n                                                     RELATORIO           \n";
+		cout << "\n **********************************************************************************************************************\n";
+		cout << "\n Código: " << Prod.Codigo;
+		cout << "\n Marca: " << Prod.Marca;
+		cout << "\n Descrição: " << Prod.Descricao;
+		cout << "\n Quantidade: " << Prod.Quantidade;
+		cout << "\n Preço de Compra: " << "R$" << Prod.PC;
+		cout << "\n Preço de Venda: " << "R$" << Prod.PV;
+	}
+}
+
+void Limpar() {
+	char opcao;
+	cout << " Deseja limpar o arquivo do relatório? (S/N)";
+	cin >> opcao;
+	if (toupper(opcao) == 'S') {
+		char opcao2;
+		cout << " CERTEZA? Você perderá todos os registros de produtos. (S/N)";
+		cin >> opcao2;
+		if (toupper(opcao2) == 'S') {
+			ArqProduto = fopen("arquivo.dat", "wb");
+			fclose(ArqProduto);
+			cout << "Arquivo removido\n";
+		}
+		system("pause");
+		system("cls");
+	}
+}
+
+void Menu() {
+	int escolha;
+
+	do {
+		cout << "\n";
+		cout << "\n **********************************************************************************************************************\n";
+		cout << "\n                                          GERENCIADOR DE PRODUTOS - MENU           \n";
+		cout << "\n **********************************************************************************************************************\n";
+		cout << " Escolha uma opção:\n";
+		cout << " 1 - Limpar\n";
+		cout << " 2 - Cadastrar\n";
+		cout << " 3 - Relatório\n";
+		cout << " 4 - Busca\n";
+		cout << " 5 - Sair\n";
+		cout << "\n **********************************************************************************************************************\n";
+		cout << "\n Opção: ";
+		cin >> escolha;
+
+		switch (escolha) {
+		case 1:
+			system("cls");
+			Limpar();
+			break;
+		case 2:
+			system("cls");
+			Cadastrar();
+			break;
+		case 3:
+			system("cls");
+			Relatorio();
+			break;
+		case 4:
+			system("cls");
+			Busca();
+			break;
+		case 5:
+			system("cls");
+			break;
+		default:
+			system("cls");
+			cout << " Opção inválida. Tente novamente.\n";
+			break;
+		}
+	} while (escolha != 5);
+}
